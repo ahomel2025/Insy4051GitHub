ff8e4948937b3f0b3eee4e45791ed7bc18241066
Adam Homel <adam.d.homel@marquette.edu>
2023-11-28 15:06:11 -0600

    Created the Model folder and the Customer Class

---
+++ Insy4051GitHub/Model/Custome.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insy4051GitHub.Model;
+
+public class Custome
+{
+}
